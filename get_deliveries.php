6beb88f06c6433f193bbabfef38bb18927226ea9
Renato Silva <renatoleite.log@gmail.com>
2024-09-22 17:08:12 +1000

    changesmade

--- get_deliveries.php
+++ get_deliveries.php
@@ -2,7 +2,7 @@
 include 'db.php';
 header('Content-Type: application/json');
 // Make sure to select the drop_number as well
-$result = $conn->query("SELECT delivery_id, street_number, street_name, suburb, latitude, longitude, drop_number FROM deliveries");
+$result = $conn->query("SELECT delivery_id, street_number, street_name, suburb, city, latitude, longitude, drop_number FROM deliveries");
 $locations = [];
 
 while ($row = $result->fetch_assoc()) {
