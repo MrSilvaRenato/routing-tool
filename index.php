56f52ece61984e7857fde7d293328c43f6e21902
Renato Silva <renatoleite.log@gmail.com>
2024-09-22 16:53:25 +1000

    changesmade

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Delivery Map</title>
   
+    <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
 </head>
 <body>
     <h2>Upload Spreadsheet</h2>
@@ -15,7 +16,7 @@
     <div id="message"></div>
     <h2>Delivery Map</h2>
     <div id="map" style="height: 500px; width: 100%;"></div>
-
+    
     <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
     <script src="js.js"></script>  <!-- External JS file -->
     
