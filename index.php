c56fee387a42daeea03d32b99d1eca60ddfaf605
Renato Silva <renatoleite.log@gmail.com>
2024-09-23 19:01:27 +1000

    changesmade

--- index.php
+++ index.php
@@ -10,8 +10,13 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script> <!-- jQuery -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script> <!-- Popper.js -->
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script> <!-- Bootstrap JS -->
-    
-  
+    <style>
+        #map {
+            height: 500px;
+            width: 100%;
+            margin-top: 20px;
+        }
+    </style>
 </head>
 <body>
     <div class="container mt-5">
@@ -19,7 +24,7 @@
 
         <div id="controls" class="d-flex justify-content-center my-3">
             <form id="uploadForm" enctype="multipart/form-data" class="form-inline">
-            <div class="custom-file mb-2 mr-2">
+                <div class="custom-file mb-2 mr-2">
                     <input type="file" name="spreadsheet" accept=".xlsx, .xls, .csv" required class="custom-file-input" id="customFile">
                     <label class="custom-file-label" for="customFile">Choose file</label>
                 </div>
@@ -34,8 +39,7 @@
 
         <div id="message" class="my-3"></div>
 
-        <h2 class="">Delivery Map</h2>
-
+        <h2 class="text-center">Delivery Map</h2>
         <div id="map"></div>
     </div>
 
