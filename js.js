2b67e7ab277a68f5e6c387de5675bab23d3a3de1
Renato Silva <renatoleite.log@gmail.com>
2024-09-25 19:18:18 +1000

    changesmade

--- js.js
+++ js.js
@@ -180,7 +180,7 @@ map.on('mouseup', function (e) {
         // Highlight selected markers
         selectedMarkers.forEach(marker => {
             marker.setIcon(new L.Icon({
-                iconUrl: 'path-to-highlighted-icon.png',  // Replace with your highlighted icon path
+                iconUrl: 'marker.png',  // Replace with your highlighted icon path
                 iconSize: [25, 41],  // Adjust size if needed
             }));
         });
